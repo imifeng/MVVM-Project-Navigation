415bf106bcdefd04540a687dbcecf01f7cda5ccd
Feng zhu <992639429@qq.com>
2023-09-01 09:50:14 +0800

    沉侵式状态栏

--- app/src/main/java/com/sxiaozhi/debt/MainActivity.kt
+++ app/src/main/java/com/sxiaozhi/debt/MainActivity.kt
@@ -1,36 +1,45 @@
 package com.sxiaozhi.debt
 
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.sxiaozhi.debt.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupWindowForStatusBar(this.window)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        navView.setupWithNavController(navController)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        setupWindow()
+        WindowInsetsControllerCompat(
+            window,
+            window.decorView
+        ).show(WindowInsetsCompat.Type.systemBars())
     }
 
-    private fun setupWindow() {
-        WindowInsetsControllerCompat(window,  window.decorView).show(WindowInsetsCompat.Type.systemBars())
+    private fun setupWindowForStatusBar(window: Window) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        val option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        val vis: Int = window.decorView.systemUiVisibility
+        window.decorView.systemUiVisibility = option or vis
+        window.statusBarColor = getColor(R.color.color_status_bar)
     }
 }
